8fcf0647234252fc72aad00e0c6a62d035f85eb6
Maxime Ricco <maxime.ricco.60@gmail.com>
2020-12-31 02:52:25 +0100

    modif js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const allItems = document.querySelectorAll("li");
 const titre = document.querySelector("h1");
 const span = document.querySelector("span");
-const btn = document.querySelector("#buttonFirst");
+const buttonFirst = document.querySelector("#buttonFirst");
 
 const TL1 = new TimelineMax({ paused: true });
 
 TL1.from(titre, 1, { y: -100, opacity: 0 }, 0.5)
   .from(span, 2, { y: -100, opacity: 0 }, 0.5)
-  .from(btn, 1, { z: -200, opacity: 0 }, 2)
+  .from(buttonFirst, 1, { z: -200, opacity: 0 }, 2)
   .staggerFrom(allItems, 1, { y: -100, opacity: 0 }, 0.7, "-=1");
 
 TL1.play();
@@ -19,10 +19,15 @@ const sr = ScrollReveal({
 
 sr.reveal("p", {
   duration: 1000,
-  delay: 500,
+  delay: 1000,
 });
 
-sr.reveal("", {
+sr.reveal(".photo", {
   duration: 1000,
-  delay: 500,
+  delay: 1000,
+});
+
+sr.reveal(".btn", {
+  duration: 1000,
+  delay: 1000,
 });
